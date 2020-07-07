9c971eee77f450af4c1f2490158a47c093a77395
Kai Volland <kaivolland@yahoo.de>
2020-07-07 10:16:10 +0200

    Adds more parses and help

--- index.ts
+++ index.ts
@@ -2,20 +2,75 @@
 
 import SLDParser from "geostyler-sld-parser";
 import QGISParser from "geostyler-qgis-parser";
+// import OpenLayersParser from "geostyler-openlayers-parser";
+import MapfileParser from "geostyler-mapfile-parser";
+import MapboxParser from "geostyler-mapbox-parser";
 import { promises as fs } from 'fs';
 import minimist from 'minimist';
 import { StyleParser } from "geostyler-style";
 
-const getParserFromString = (inputString: string): StyleParser =>{
+const getParserFromFormat = (inputString: string): StyleParser =>{
+  if (!inputString) {
+    return undefined;
+  }
   switch (inputString.toLowerCase()) {
+    // case 'openlayers':
+    // case 'ol':
+    //   return new OpenLayersParser();
+    case 'mapbox':
+      return new MapboxParser();
+    case 'mapfile':
+    case 'map':
+      return new MapfileParser();
     case 'sld':
       return new SLDParser();
     case 'qgis':
+    case 'qml':
       return new QGISParser();
     default:
-      return new SLDParser();
+      return undefined;
   }
 }
+
+const getParserFromFilename = (fileName: string): StyleParser =>{
+  if (!fileName) {
+    return undefined;
+  }
+  const fileEnding = fileName.split('.')[1];
+  switch (fileEnding.toLowerCase()) {
+    // case 'ol':
+    //   return new OpenLayersParser();
+    case 'mapbox':
+      return new MapboxParser();
+    case 'map':
+      return new MapfileParser();
+    case 'sld':
+      return new SLDParser();
+    case 'qml':
+      return new QGISParser();
+    default:
+      return undefined;
+  }
+}
+
+const printHelp = () => {
+  console.log(`
+  ###################################
+  # GeoStyler Commandline Interface #
+  ###################################
+
+  Basic syntax:
+    npm start -- [options] inputfile
+  Example:
+    npm start -s sld -t mapfile -o myStyle.map testdata/point_simplepoint.sld
+
+  Options:
+    -s / --source : SourceParser. ["mapbox" | "mapfile" | "sld" | "qgis"]
+    -t / --target : TargetParser. ["mapbox" | "mapfile" | "sld" | "qgis"]
+    -o / --output : Outpuft filename. [string]
+    -h / --help: Display this help.
+  `);
+};
 
 async function main() {
   const args = minimist(process.argv.slice(2));
@@ -26,28 +81,50 @@ async function main() {
     target,
     o,
     output,
+    h,
+    help,
     _: unnamedArgs
   } = args;
 
-  const inputFile:string = unnamedArgs[0];
-  const sourceFormat: string = s || source;
-  const targetFormat: string = t || target;
-  const outPutFile: string = o || output;
-
-  console.log(`Transforming file ${inputFile} from ${sourceFormat} to ${targetFormat}`);
-  if (outPutFile) {
-    console.log(`… output will be written to ${outPutFile}`);
+  if (h || help) {
+    printHelp();
+    return;
   }
 
-  let sourceParser = getParserFromString(sourceFormat);
-  let targetParser = getParserFromString(targetFormat)
-  const inputFileData = await fs.readFile(inputFile, 'utf-8');
+  const sourceFile:string = unnamedArgs[0];
+  const sourceFormat: string = s || source;
+  const targetFormat: string = t || target;
+  const targetFile: string = o || output;
+
+  if (!sourceFile) {
+    console.log('No input file specified.');
+    console.log('USE: `npm start -- [options] inputfile`');
+    return;
+  }
+
+  let sourceParser = getParserFromFormat(sourceFormat) || getParserFromFilename(sourceFile);
+  let targetParser = getParserFromFormat(targetFormat) || getParserFromFilename(targetFile)
+  const inputFileData = await fs.readFile(sourceFile, 'utf-8');
+
+  if (!sourceParser) {
+    console.log('No sourceparser was specified.');
+    return;
+  }
+  if (!targetParser) {
+    console.log('No targetparser was specified.');
+    return;
+  }
+
+  console.log(`Transforming file ${sourceFile} from ${sourceFormat} to ${targetFormat}`);
+  if (targetFile) {
+    console.log(`… output will be written to ${targetFile}`);
+  }
 
   try {
     const geostylerStyle = await sourceParser.readStyle(inputFileData);
     const targetStyle = await targetParser.writeStyle(geostylerStyle);
-    if (outPutFile) {
-      fs.writeFile(outPutFile, targetStyle, 'utf-8');
+    if (targetFile) {
+        fs.writeFile(targetFile, targetStyle, 'utf-8');
     } else {
       console.log(targetStyle);
     }
