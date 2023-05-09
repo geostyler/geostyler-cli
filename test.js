9473beda18be22699279a9bd3b169b549c9a534a
sethg <sethg@geographika.co.uk>
2023-05-09 15:32:16 +0200

    Return exit code and run as synch

--- test.js
+++ test.js
@@ -1,49 +1,38 @@
 const fs = require('fs');
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 
 function checkFileCreated(outputFile) {
   try {
     fs.accessSync(path, fs.constants.F_OK);
     return true;
   } catch (err) {
+    /* eslint-disable no-console */
+    console.log(`The file: ${outputFile} was not found`);
     return false;
   }
 }
 
 function runTest(args, outputFile) {
   const cmd = 'npm';
-  const child = spawn(cmd, args, { shell: true });
-
-  child.on('error', (err) => {
-    /* eslint-disable no-console */
-    console.error(`Child process error: ${err}`);
-  });
-
-  child.on('exit', (code) => {
-    console.log(`Child process exited with code ${code}`);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.log(`Error: ${data}`);
-  });
-
-  child.stdout.on('data', (data) => {
-    console.log(`Output: ${data}`);
-  });
+  const result = spawnSync(cmd, args, { shell: true });
+  /* eslint-disable no-console */
+  console.log(`Status: ${result.status.toString()}`);
+  console.log(`Output: ${result.stdout.toString()}`);
+  console.log(`Error: ${result.stderr.toString()}`);
 
   return checkFileCreated(outputFile);
 }
 
 function runAllTests() {
 
-  let passed = true;
+  let success = true;
 
   // test sld to qgis
   let outputFile = 'output.qml';
   let args = ['start', '--', '-s', 'sld', '-t', 'qgis', '-o', outputFile, 'testdata/point_simplepoint.sld'];
 
   if (runTest(args, outputFile) === false) {
-    passed = false;
+    success = false;
   }
 
   // test qgis to sld
@@ -51,7 +40,7 @@ function runAllTests() {
   args = ['start', '--', '-s', 'qgis', '-t', 'sld', '-o', outputFile, 'testdata/point_simple.qml'];
 
   if (runTest(args, outputFile) === false) {
-    passed = false;
+    success = false;
   }
 
   // test mapfile to geostyler
@@ -59,10 +48,12 @@ function runAllTests() {
   args = ['start', '--', '-s', 'mapfile', '-o', outputFile, 'testdata/point_simplepoint.map'];
 
   if (runTest(args, outputFile) === false) {
-    passed = false;
+    success = false;
   }
 
-  return passed;
+  return success;
 }
 
-return runAllTests();
+if (runAllTests() === false) {
+  process.exit(1);
+}
